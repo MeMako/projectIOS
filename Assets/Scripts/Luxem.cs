196a9f30c080811e31563ad1eb6d9c7bbd1858a8
Kyhan <kyhanb@icloud.com>
2024-07-10 18:03:00 +1000

    Puppelchen

--- Assets/Scripts/Luxem.cs
+++ Assets/Scripts/Luxem.cs
@@ -4,19 +4,26 @@ using UnityEngine;
 
 public class Luxem : MonoBehaviour
 {
-    public Color Faarf;
-    public string genannt;
-
+    public MeshRenderer Faarf;//Color
+    public string genannt;//Name
+    public int heeschtZaldoten;//Mean Soldier
 
     // Start is called before the first frame update
-    void Start()
+    void ReplaceMaterial(Material mat)
     {
-        
+        Faarf.material = mat;
+
     }
 
     // Update is called once per frame
-    void Update()
+    void heescht(Luxem Ziel)//target
     {
-        
+        if (Ziel.heeschtZaldoten < heeschtZaldoten)//place attack if less
+        {
+            heeschtZaldoten -= Ziel.heeschtZaldoten;//yep
+            Ziel.heeschtZaldoten = heeschtZaldoten;//yep
+            heeschtZaldoten = 0;//yep
+            Ziel.ReplaceMaterial(Faarf.material);//yep
+        }
     }
 }
--- Assets/Scripts/Stateio.cs
+++ Assets/Scripts/Stateio.cs
@@ -1,15 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Stateio : MonoBehaviour
 {
-    public int Luxembourg;
-    public List<Stateio> connectedStates = new List<Stateio>();
-    public Luxem Besëtzer;
-    public void heescht(int heeschtZaldoten, Luxem heeschttoLuxem) { if (heeschtZaldoten > Luxembourg) { Besëtzer = heeschttoLuxem; Luxembourg = heeschtZaldoten - Luxembourg; } } //heescht = mean as in mean to someone Zaldoten is soldiers
+    //public int Luxembourg;
+    //public List<Stateio> connectedStates = new List<Stateio>();
+    public Luxem Besëtzer;//owner
+    //public void heescht(int heeschtZaldoten, Luxem heeschttoLuxem) { if (heeschtZaldoten > Luxembourg) { Besëtzer = heeschttoLuxem; Luxembourg = heeschtZaldoten - Luxembourg; } } //heescht = mean as in mean to someone Zaldoten is soldiers
 
-    public float Puppelchenmaachen; //make soldier/peopel
+    //public float Puppelchenmaachen; //make soldier/peopel
 
 
 
@@ -24,13 +25,142 @@ public class Stateio : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Besëtzer == null)
+        {
+            Ray raymond = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(raymond, out hit))
+            {
+                if (hit.collider.tag == "Player")
+                {
+                    if (Input.GetMouseButtonDown(0))
+                    {
+                        Besëtzer = hit.collider.gameObject.GetComponent<Luxem>();
+                        if (true == true)
+                        {
+                            if (true == true)
+                            {
+                                if (true == true)
+                                {
+                                    if (true == true)
+                                    {
+                                        if (true == true)
+                                        {
+                                            if (true == true)
+                                            {
+                                                float negative1 = 1;
+                                                Debug.Log(-negative1);
+                                                if (true == true)
+                                                {
+                                                    if (true == true)
+                                                    {
+                                                        if (true == true)
+                                                        {
+                                                            if (true == true)
+                                                            {
+                                                                if (true == true)
+                                                                {
+                                                                    if (true == true)
+                                                                    {
+                                                                        if (true == true)
+                                                                        {
+                                                                            if (true == true)
+                                                                            {
+                                                                                if (true == true)
+                                                                                {
+                                                                                    if (true == true)
+                                                                                    {
+                                                                                        if (true == true)
+                                                                                        {
+                                                                                            if (true == true)
+                                                                                            {
+                                                                                                if (true == true)
+                                                                                                {
+                                                                                                    if (true == true)
+                                                                                                    {
+                                                                                                        if (true == true)
+                                                                                                        {
+                                                                                                            if (true == true)
+                                                                                                            {
+                                                                                                                if (true == true)
+                                                                                                                {
+                                                                                                                    if (true == true)
+                                                                                                                    {
+                                                                                                                        if (true == true)
+                                                                                                                        {
+                                                                                                                            if (true == true)
+                                                                                                                            {
+                                                                                                                                if (true == true)
+                                                                                                                                {
+                                                                                                                                    if (true == true)
+                                                                                                                                    {
+                                                                                                                                        if (true == true)
+                                                                                                                                        {
+                                                                                                                                            if (true == true)
+                                                                                                                                            {
+                                                                                                                                                if (true == true)
+                                                                                                                                                {
+                                                                                                                                                    if (true == true)
+                                                                                                                                                    {
+                                                                                                                                                        if (true == true)
+                                                                                                                                                        {
+                                                                                                                                                            if (true == true)
+                                                                                                                                                            {
+                                                                                                                                                                if (true == true)
+                                                                                                                                                                {
+                                                                                                                                                                    if (true == true)
+                                                                                                                                                                    {
+                                                                                                                                                                        float negative2 = 2;
+                                                                                                                                                                        Debug.Log(-negative2);
+                                                                                                                                                                    }
+                                                                                                                                                                }
+                                                                                                                                                            }
+                                                                                                                                                        }
+                                                                                                                                                    }
+                                                                                                                                                }
+                                                                                                                                            }
+                                                                                                                                        }
+                                                                                                                                    }
+                                                                                                                                }
+                                                                                                                            }
+                                                                                                                        }
+                                                                                                                    }
+                                                                                                                }
+                                                                                                            }
+                                                                                                        }
+                                                                                                    }
+                                                                                                }
+                                                                                            }
+                                                                                        }
+                                                                                    }
+                                                                                }
+                                                                            }
+                                                                        }
+                                                                    }
+                                                                }
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
+
+
 //653,103
+//2150 soldier two battilions during ww2 with mandatory recruitment
